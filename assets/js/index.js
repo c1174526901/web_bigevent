1e2da67d61887be0c69fc758b38995cc5a45bb69
chenyao <cy_888@163.com>
2020-11-29 20:44:14 +0800

    完成了个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,18 +20,6 @@ $(function () {
                 renderAvatar(res.data)
             },
             // 不论成功还是失败，最终都会调用 complete 回调函数
-            complete: function(res) {
-              // console.log('执行了 complete 回调：')
-              // console.log(res)
-              // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-              if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-                // 1. 强制清空 token
-                localStorage.removeItem('token')
-                // 2. 强制跳转到登录页面
-                location.href = 'login.html'
-              }
-            }
-
         })
     }
 //渲染用户头像
@@ -40,6 +28,7 @@ $(function () {
         var name = user.nickname || user.username
         // 2. 设置欢迎的文本
         $('#weclome').html('欢迎&nbsp;&nbsp;' + name)
+        var first = name[0].toUpperCase()
         // 3. 按需渲染用户的头像
         if (user.user_pic !== null) {
             // 3.1 渲染图片头像
@@ -50,8 +39,8 @@ $(function () {
         } else {
             // 3.2 渲染文本头像
             $('.layui-nav-img').hide()
-            var first = name[0].toUpperCase()
-            $('.text-avatar')
+
+            $('.text-avater')
                 .html(first)
                 .show()
         }
